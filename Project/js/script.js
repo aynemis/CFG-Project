ef4f0c81a72d8bf90df9a4f9a2ebcb16825041d0
JuliaZofiaCodes <100809673+JuliaZofiaCodes@users.noreply.github.com>
2022-10-03 15:22:55 +0100

    Added two arrays with names

---
+++ Project/js/script.js
@@ -0,0 +1,4 @@
+let female_names =
+  "Amanda Beatrice Catherine Daphne Edwina Frances Grace Harriet Imogen Jemima Katherine Lucy MargaretNancy Olivia Poppy Quintana Rosemary Sarah Tracy Ursula Victoria Willa Xanthe Yseult Zara";
+let male_names =
+  "Albert Benjamin Charles David Edward Francis Giles Henry Inigo Jonathan Kenneth Lionel Miles Nathaniel Oliver Patrick Quentin Rupert Sebastian Thomas Unwin Vere William Xerxes Youngblood Zach";
