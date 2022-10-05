57ffe176a0a6c673bde56a6061a258f00e6ef984
aynemis <111521209+aynemis@users.noreply.github.com>
2022-10-05 18:28:28 +0200

    Update script.js

--- Project/js/script.js
+++ Project/js/script.js
@@ -1,86 +1,51 @@
-// Those are strings of names, titles, surnames needs to be added
-let female_names =
-  "Amanda Beatrice Catherine Daphne Edwina Frances Grace Harriet Imogen Jemima Katherine Lucy Margaret Nancy Olivia Poppy Quintana Rosemary Sarah Tracy Ursula Victoria Willa Xanthe Yseult Zara";
-let male_names =
-  "Albert Benjamin Charles David Edward Francis Giles Henry Inigo Jonathan Kenneth Lionel Miles Nathaniel Oliver Patrick Quentin Rupert Sebastian Thomas Unwin Vere William Xerxes Youngblood Zach";
-let female_titles = "Lady Baroness";
-let male_titles = "Sir Baron";
+const firstNames = [
+  ['Henry', 'William', 'Louis'],
+  ['Mary', 'Laura', 'Veronica']
+];
 
-// Those are lists made out of strings above
+const secondNames = [
+  ['Peter', 'Fred', 'Matthew'],
+  ['Elizabeth', 'Helena', 'Aurora']
+];
 
-let female_list = female_names.split(" ");
-let male_list = male_names.split(" ");
-let female_titles_list = female_titles.split(" ");
-let male_titles_list = male_titles.split(" ");
+const thirdNames = ['de la Mare', 'de la Grande Valee', 'The Third'];
 
+const output = document.querySelector('.generated-name');
+const radios = document.getElementsByName('number');
+const genders = document.getElementById('genders');
+const btn_generate = document.querySelector('.btn-generate');
+         
+btn_generate.addEventListener('click', function() {
+let selectedRadio = '';
+let selectedGender = '';
 
-console.log(document.querySelector('input[name="gender"]:checked').value);
-// // console.log(document.getElementById("generator").submit());
-// var gender = document.getElementsByName('gender');
-// var aristocrat = document.getElementsByName('aristocrat');
-
-function namegenerator(aristocrat, gender) {
-  var female_list_number = Math.round(Math.random() * (female_list.length - 1));
-  var male_list_number = Math.round(Math.random() * (male_list.length - 1));
-  var femtitles_list_number = Math.round(
-    Math.random() * (female_titles_list.length - 1)
-  );
-  var maletitles_list_number = Math.round(
-    Math.random() * (male_titles_list.length - 1)
-  );
-  if (aristocrat == true) {
-    if (gender == "Female") {
-      let goodname =
-        female_titles_list[femtitles_list_number] +
-        " " +
-        female_list[female_list_number];
-      return goodname;
-    } else if (gender == "Male") {
-      let goodname =
-        male_titles_list[maletitles_list_number] +
-        " " +
-        male_list[male_list_number];
-      return goodname;
-    } else {
-      let genders = ["Female", "Male"];
-      let random_gender = genders[Math.round(Math.random())];
-      if (random_gender == "Female") {
-        let goodname =
-          female_titles_list[femtitles_list_number] +
-          " " +
-          female_list[female_list_number];
-        return goodname;
-      } else {
-        let goodname =
-          male_titles_list[maletitles_list_number] +
-          " " +
-          male_list[male_list_number];
-        return goodname;
-      }
-    }
-  } 
-  else {
-    if (gender == "Female") {
-      let goodname = female_list[female_list_number];
-      return goodname;
-    } else if (gender == "Male") {
-      let goodname = male_list[male_list_number];
-      return goodname;
-    } else {
-      let genders = ["Female", "Male"];
-      let random_gender = genders[Math.round(Math.random())];
-      if (random_gender == "Female") {
-        let goodname = female_list[female_list_number];
-        return goodname;
-      } else {
-        let goodname =  male_list[male_list_number];
-        return goodname;
-      }
-    }
+for(var i = 0; i < radios.length; i++) {
+  if(radios[i].checked) {
+    selectedRadio = radios[i].value;
   }
 }
 
-// for (i=0; i<11; i++){
-//   console.log(namegenerator(true, "Female"));
-//   console.log(namegenerator(false, "Male"));
-// }
+for(var i = 0; i < genders.length; i++) {
+  if(genders[i].selected) {
+    selectedGender = genders[i].value;
+  }
+}
+
+let genderID = selectedGender === 'Male' ? 0 : 1 ;
+
+switch(selectedRadio) {
+
+  case 'one':
+    output.value = firstNames[genderID][Math.floor(Math.random() * firstNames[genderID].length)];
+    break;
+  case 'two':
+    output.value = firstNames[genderID][Math.floor(Math.random() * firstNames[genderID].length)]
+      + ' ' + secondNames[genderID][Math.floor(Math.random() * secondNames[genderID].length)];
+    break;
+  case 'three':
+    output.value = firstNames[genderID][Math.floor(Math.random() * firstNames[genderID].length)]
+      + ' ' + secondNames[genderID][Math.floor(Math.random() * secondNames[genderID].length)]
+    + ' ' + thirdNames[Math.floor(Math.random() * thirdNames.length)];
+    break;
+}
+});
